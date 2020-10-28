61ccb13aaa75b00c8a3277c572dc4858312b8974
matheus73 <matheuscoding@gmail.com>
2020-10-28 17:03:42 -0300

    new configures in polybar and i3

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/zsh
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,12 +8,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch example
 # polybar example &
-
-# polybar -q shadbar &
-polybar shadbar &
-# polybar -q shadbar2 &
-polybar shadbar2 &
-# echo "Bars launched..."
-polybar hdmi &
-
-polybar hdmi2 &
+polybar shadbar2 -r &
+polybar hdmi2 -r &
